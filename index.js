0ae7e5e30afc8527af140dd9bb6fdae11cd3aaeb
Priti <pal171747@gmail.com>
2024-04-08 16:49:46 +0530

    end

--- index.js
+++ index.js
@@ -1,11 +1,10 @@
-let burgar=document.querySelectorAll(".burgar");
-let nav=document.querySelector(".nav");
-let rightnav=document.querySelector(".rightnav");
-let navlist=document.querySelector(".navlist");
+let rightnav=document.querySelector('.rightnav')
+let burgar=document.querySelector('.burgar')
+let navlist=document.querySelector('.navlist')
+let nav=document.querySelector('.nav')
 
-burgar.addEventListener("click",()=>{
-    navlist.classList.toggle("op-res");
-    rightnav.classList.toggle("op-res");
-    nav.classList.toggle("h-nav-res");
-
-});
+burgar.addEventListener('click',()=>{
+nav.classList.toggle('h-nav-res');
+navlist.classList.toggle('op-res');
+rightnav.classList.toggle('op-res');
+})
